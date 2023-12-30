007930c2749d4504fc2d2a35d5cac68ff3c1168d
CatalinRotaru8 <catalin.rotaru@estudiante.uam.es>
2023-12-30 20:04:29 +0100

    add the data cleaned to the processed data folder

--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,5 @@
 """Main module for the TFG project."""
-from src.utils.utils import (
-    get_file_path,
-    delete_duplicate_line_from_file,
-    change_header_from_file,
-    delete_last_column_from_file,
-    clean_all_data,
-    transform_file,
-)
-from src.config import CleanData, DeleteLines, FileName
-import pandas as pd
-from src.utils.fbref import Fbref
-from src.utils.downloader import get_big5_data
+from src.core.controller import clean_all_data
 
 
 def main():
@@ -18,7 +7,6 @@ def main():
     Main function for the project
     """
     # get_big5_data(Fbref.stats_dict)
-
     clean_all_data()
 
 
--- src/core/controller.py
+++ src/core/controller.py
@@ -1,1 +1,10 @@
 """Controller module"""
+from src.utils.fbref import Fbref
+from src.utils.downloader import get_big5_data
+from src.utils.utils import clean_all_data
+
+
+def download_clean_data():
+    """Download and clean all the data"""
+    get_big5_data(Fbref.stats_dict)
+    clean_all_data()
